22f88d04a067fc43c14d59285b842be2931c0af2
Shaan Sandhu <s_sandhu@umail.ucsb.edu>
2019-09-03 00:53:01 -0700

    Updates

---
+++ optics.py
@@ -0,0 +1,9 @@
+class SolarProjector():
+    def __init__(self,focal_length1,focal_length2,length):
+        self.focal_legnth1=focal_length1
+        self.focal_length2=focal_length2
+        self.length=length
+    def find_magnitude(self):
+        self.magnitude=#INSERT QUADRATIC FORMULA HERE
+        return magnitude
+

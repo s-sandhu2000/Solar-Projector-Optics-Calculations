1cb8a5db5d8c24d50473349d08995f65e8e372ad
Shaan Sandhu <s_sandhu@umail.ucsb.edu>
2019-09-04 00:11:29 -0700

    Made program more robust

--- opticalCalc.cpp
+++ opticalCalc.cpp
@@ -15,12 +15,6 @@ int main(int argc,char*argv[])
 	double a=focal_length2;
 	double b=((2*focal_length2)+focal_length1-total_length);
 	double c=focal_length2;
-/*	cout<<"Focal Length 1: "<<focal_length1<<endl;
-	cout<<"Focal Length 2: "<<focal_length2<<endl;
-	cout<<"Total Length: "<<total_length<<endl;
-	cout<<"a: "<<a<<endl;
-	cout<<"b: "<<b<<endl;
-	cout<<"c: "<<c<<endl;*/
 	double solution1=((-b)+sqrt((pow(b,2))-(4*a*c)))/(2*a);
 	double solution2=((-b)-sqrt((pow(b,2))-(4*a*c)))/(2*a);
 	cout<<solution1<<endl;
@@ -28,7 +22,7 @@ int main(int argc,char*argv[])
 	double og_image=((1.39*pow(10,9))/(1.50*pow(10,11)))*focal_length1;
 	if (solution1 >=0)
 	{
-		cout<<"Magnification option 1:"<<solution1<<endl;
+		cout<<"Magnification option 1: "<<solution1<<endl;
 		cout<<endl;
 		double o=(((total_length)-(focal_length1))/(solution1+1))+focal_length1;
 		double i=(solution1*o);
@@ -40,7 +34,7 @@ int main(int argc,char*argv[])
 		cout<<"Negative answer"<<endl;
 	if (solution2 >=0)
 	{
-		cout<<"Magnification option 2:"<<solution2<<endl;
+		cout<<"Magnification option 2: "<<solution2<<endl;
 		cout<<endl;
 		double o=(((total_length)-(focal_length1))/(solution2+1))+focal_length1;
 		double i=(solution2*o);
@@ -49,8 +43,13 @@ int main(int argc,char*argv[])
 	}
 	else
 		cout<<"Negative answer"<<endl;
-	double max=((solution1>solution2)? solution1:solution2);
-	cout<<"Overall, the image of the sun is going to be "<<(max*og_image)<<" meters tall."<<endl;
+	if((solution1>=0) || (solution2>=0))
+	{
+		
+		double max=((solution1>solution2)? solution1:solution2);
+		cout<<"We are going to be working with magnification "<<max<<"."<<endl;
+		cout<<"Overall, the image of the sun is going to be "<<(max*og_image)<<" meters tall."<<endl;
+	}
 	return 0;
 }
 
